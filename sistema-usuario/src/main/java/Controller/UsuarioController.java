c537f1b6901f431058a4266655a1d8074d0c0e97
edupr12 <edupr12@hotmail.com>
2017-02-21 12:45:13 -0300

    Aula HTTP

---
+++ sistema-usuario/src/main/java/Controller/UsuarioController.java
@@ -0,0 +1,26 @@
+package Controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns="/usucontroller")
+public class UsuarioController extends HttpServlet {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	//capturando o que vem do cliente
+	String email = req.getParameter("email");
+	//Respondendo ao cliente
+	resp.getWriter().print("  ola " + email );
+  }
+
+}
